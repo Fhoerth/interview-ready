2772ce9fd463a16cf8b81cf2af3ca972d13cceda
Fhoerth <federicohoerth@gmail.com>
2025-04-21 20:37:24 -0300

    Add removeDups implementation

--- technical-fundamentals/coding/problems/11_removeDups.ts
+++ technical-fundamentals/coding/problems/11_removeDups.ts
@@ -3,13 +3,24 @@
 // Write code to remove duplicates from an unsorted linked list. FOLLOW UP
 // How would you solve this problem if a temporary buffer is not allowed?
 //
-// 1 -> 2 -> 2-> 2 -> 4
+// 1 -> 2 -> 2 -> 2 -> 4
 
-import { LinkedList } from './10_LinkedList';
+import { Node, LinkedList } from '../ds/LinkedList';
 
-export type Node<T> = {
-  value: T;
-  next?: Node<T>;
-};
+export { Node };
 
-export default function removeDups<T>(head?: Node<T>): Node<T> | undefined {}
+export default function removeDups<T>(head?: Node<T>): Node<T> | undefined {
+  const seen = new Set<T>(); // O(1)
+  const list = new LinkedList(head); // O(n)
+
+  for (const handler of list) { // O(n)
+    if (seen.has(handler.value())) { // O(1)
+      handler.remove(); // O(1)
+      continue;
+    }
+
+    seen.add(handler.value()); // O(1)
+  }
+
+  return list.unsafeHead();
+}
