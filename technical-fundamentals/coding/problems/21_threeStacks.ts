4037bc411bb9279d80246b9e444f21cc24c16bb2
Fhoerth <federicohoerth@gmail.com>
2025-04-22 19:08:44 -0300

    Add threeStacks impl

--- technical-fundamentals/coding/problems/21_threeStacks.ts
+++ technical-fundamentals/coding/problems/21_threeStacks.ts
@@ -1,13 +1,61 @@
 // 1. *Three in One*: Describe how you could use a single array to implement three stacks.
-
+import { assert } from "../utils/assert";
 export default class ThreeStacks<T> {
-  private array: T[];
+  private array: (T | null)[] = [];
+  private nextIndexes = new Map<number, number>();
+  private numStacks = 3;
+  
+  constructor(arrayLength: number) {
+    for (let j = 0; j < arrayLength; j += 1) {
+      this.array.push(null);
+    }
 
-  constructor(arrayLength: number) {}
+    for (let j = 0; j < this.numStacks; j += 1) {
+      this.nextIndexes.set(j, j);
+    }
+  }
 
-  push(stackNum: number, value: T): void {}
+  private getStackCurrentIdx(stackNum: number): number {
+    const nextIdx = this.nextIndexes.get(stackNum);
+    assert(nextIdx);
 
-  pop(stackNum: number): T | undefined {}
+    const currentIdx = nextIdx - this.numStacks;
 
-  peek(stackNum: number): T | undefined {}
+    return currentIdx;
+  }
+
+  push(stackNum: number, value: T): void {
+    const nextIdx = this.nextIndexes.get(stackNum);
+    assert(nextIdx);
+
+    this.array[nextIdx] = value;
+    this.nextIndexes.set(stackNum, nextIdx + this.numStacks);
+  }
+
+  pop(stackNum: number): T | undefined {
+    const currentIdx = this.getStackCurrentIdx(stackNum);
+
+    if (currentIdx < 0) {
+      return undefined;
+    }
+
+    const item = this.array[currentIdx];
+
+    this.nextIndexes.set(stackNum, currentIdx);
+    this.array[currentIdx] = null;
+
+    return item === null ? undefined : item;
+  }
+
+  peek(stackNum: number): T | undefined {
+    const currentIdx = this.getStackCurrentIdx(stackNum);
+
+    if (currentIdx < 0) {
+      return undefined;
+    }
+
+    const item = this.array[currentIdx];
+
+    return item === null ? undefined : item;
+  }
 }
