9eb8294ddce4bb8e2243e4fb709d0995552857d2
Fhoerth <federicohoerth@gmail.com>
2025-04-22 19:30:59 -0300

    Add stackMin impl

--- technical-fundamentals/coding/problems/22_stackMin.ts
+++ technical-fundamentals/coding/problems/22_stackMin.ts
@@ -4,12 +4,41 @@
 // Push, pop, and min should all operate in O(1) time.
 //
 
-export default class StackMin<T> {
+
+export default class StackMin<T extends number> {
   constructor() {}
+  #minStack: number[] = [Infinity];
+  #stack: number[] = [];
 
-  push(value: T): void {}
+  #currentMin(): number {
+    return this.#minStack[this.#minStack.length - 1];
+  }
 
-  pop(): T | undefined {}
+  push(value: T): void {
+    if (value <= this.#currentMin()) {
+      this.#minStack.push(value);
+    }
 
-  min(): T | undefined {}
+    this.#stack.push(value);
+  }
+
+  pop(): number | undefined {
+    if (!this.#stack.length)
+      return undefined;
+    
+    const item = this.#stack.pop();
+
+    if (item === this.#currentMin()) {
+      this.#minStack.pop();
+    }
+
+    return item;
+  }
+
+  min(): number | undefined {
+    if (!this.#stack.length)
+      return undefined;
+
+    return this.#currentMin();
+  }
 }
