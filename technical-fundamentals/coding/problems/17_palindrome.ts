8a5398491d99a734c755b3921eaa487d3ec8877b
Fhoerth <federicohoerth@gmail.com>
2025-04-21 23:11:42 -0300

    Add palindrome impl

--- technical-fundamentals/coding/problems/17_palindrome.ts
+++ technical-fundamentals/coding/problems/17_palindrome.ts
@@ -2,11 +2,19 @@
 
 // Implement a function to check if a linked list is a palindrome.
 
-import { LinkedList } from './10_LinkedList';
+import { Node, LinkedList, Handler } from '../ds/LinkedList';
+export { Node };
 
-export type Node<T> = {
-  value: T;
-  next?: Node<T>;
-};
+export default function isPalindrome<T>(head: Node<T> | undefined): boolean {
+  const list = new LinkedList<T>(head);
 
-export default function isPalindrome<T>(head: Node<T> | undefined): boolean {}
+  let handler1: Handler<T> | undefined;
+
+  for (const handler of list) handler1 = handler;
+  for (const handler2 of list) {
+    if (handler1?.value() != handler2.value()) return false;
+    handler1 = handler1.previous();
+  }
+
+  return true;
+}
