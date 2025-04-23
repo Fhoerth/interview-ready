1e58c4d74941810ddf3b4b3a8ab77eea076e8c75
Fhoerth <federicohoerth@gmail.com>
2025-04-23 08:57:32 -0300

    Add queueViaStacks impl

--- technical-fundamentals/coding/problems/24_queueViaStacks.ts
+++ technical-fundamentals/coding/problems/24_queueViaStacks.ts
@@ -1,15 +1,52 @@
 // 4. *Queue via Stacks*:
 
 // Implement a MyQueue class which implements a queue using two stacks.
+import { L } from 'vitest/dist/chunks/reporters.C_zwCd4j.js';
+import { Stack } from '../ds/Stack';
 
 export default class MyQueue<T> {
   constructor() {}
 
-  enqueue(value: T): void {}
+  #stacks: [Stack<T>, Stack<T>] = [new Stack<T>(), new Stack<T>()];
+  #idx = 0;
 
-  dequeue(): T | undefined {}
+  #currentIdx(): number {
+    return this.#idx % 2;
+  }
 
-  peek(): T | undefined {}
+  #otherIdx(): number {
+    return (this.#idx + 1) % 2;
+  }
 
-  isEmpty(): boolean {}
+  enqueue(value: T): void {
+    const currentStack = this.#stacks[this.#currentIdx()];
+    const otherStack = this.#stacks[this.#otherIdx()];
+
+    otherStack.push(value);
+    otherStack.prepend(currentStack);
+
+    this.#stacks[this.#currentIdx()] = new Stack<T>();
+    this.#idx += 1;
+  }
+
+  dequeue(): T | undefined {
+    const currentStack = this.#stacks[this.#currentIdx()];
+
+    if (!currentStack.size())
+      return undefined;
+
+    const element = currentStack.pop();
+
+    return element;
+  }
+
+  peek(): T | undefined {
+    const currentStack = this.#stacks[this.#currentIdx()];
+    return currentStack.peek();
+  }
+
+  isEmpty(): boolean {
+    const currentStack = this.#stacks[this.#currentIdx()];
+    return !currentStack.size();
+  }
 }
