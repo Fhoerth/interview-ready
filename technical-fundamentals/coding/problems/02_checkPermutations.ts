0c7189d777cf079cb8f7c3dbe98a738e9939bfca
Fhoerth <federicohoerth@gmail.com>
2025-04-23 19:32:49 -0300

    Add checkPermutations impl

--- technical-fundamentals/coding/problems/02_checkPermutations.ts
+++ technical-fundamentals/coding/problems/02_checkPermutations.ts
@@ -2,4 +2,23 @@
 
 // Given two strings, write a method to decide if one is a permutation of the other.
 
-export default function checkPermutations(s1: string, s2: string): boolean {}
+export default function checkPermutations(s1: string, s2: string): boolean {
+  const freq = new Map<string, number>();
+
+  for (const char of s1) {
+    const count = freq.get(char);
+    const charFreq = count === undefined ? 1 : count + 1;
+
+    freq.set(char, charFreq);
+  }
+
+  for (const char of s2) {
+    const count = freq.get(char);
+
+    if (count === undefined) return false;
+    else if (count === 1) freq.delete(char);
+    else freq.set(char, count - 1);
+  }
+
+  return !freq.size;
+}
