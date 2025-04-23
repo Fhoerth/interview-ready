aa5ab48310c96f369cabe0b53cf85dbbde225008
Fhoerth <federicohoerth@gmail.com>
2025-04-23 19:51:36 -0300

    Add urlify impl

--- technical-fundamentals/coding/problems/03_urlify.ts
+++ technical-fundamentals/coding/problems/03_urlify.ts
@@ -4,4 +4,18 @@
 // You may assume that the string has sufficient space at the end to hold the additional characters,
 // and that you are given the "true" length of the string.
 
-export default function URLify(s1: string): string {}
+/**
+ * Note:
+ * This could be done in-place if the input string were represented as a character array
+ * with enough trailing space to accommodate the additional characters.
+ * In that scenario, and if the true length of the string were provided,
+ * we could perform the transformation from the end of the array backwards,
+ * replacing spaces with '%20' without allocating extra memory.
+ *
+ * However, in JavaScript, strings are immutable, and the input here
+ * does not include the extra space or true length. Therefore, we simply
+ * return a new string with spaces replaced.
+ */
+export default function URLify(s1: string): string {
+  return s1.replace(/ /g, '%20');
+}
