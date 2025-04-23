a6b4828e6838f90ee596e2e4a0db7355a030f1f6
Fhoerth <federicohoerth@gmail.com>
2025-04-23 19:18:13 -0300

    Add isUnique impl

--- technical-fundamentals/coding/problems/01_isUnique.ts
+++ technical-fundamentals/coding/problems/01_isUnique.ts
@@ -3,4 +3,13 @@
 // Implement an algorithm to determine if a string has all unique characters.
 // What if you cannot use additional data structures?
 
-export default function isUnique(str: string): boolean {}
+export default function isUnique(str: string): boolean {
+  const chars = new Set<string>();
+
+  for (const char of str) {
+    if (chars.has(char)) return false;
+    chars.add(char);
+  }
+
+  return true;
+}
