43ce97801a18b346a7b20ed77374fffd174a4971
Fhoerth <federicohoerth@gmail.com>
2025-04-23 22:08:42 -0300

    Add dfs/bfs impl

--- technical-fundamentals/coding/problems/30_trees.ts
+++ technical-fundamentals/coding/problems/30_trees.ts
@@ -1,4 +1,5 @@
 // Write the basic tree algorithms of Depth-first-search and Breadth-first search.
+import { Queue } from '../ds/Queue';
 
 export type TreeNode<T> = {
   value: T;
@@ -7,7 +8,37 @@ export type TreeNode<T> = {
 };
 
 export class Tree<T> {
-  bfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {}
+  bfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {
+    const queue = new Queue<TreeNode<T> | undefined>();
 
-  dfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {}
+    queue.enqueue(node);
+
+    while (!queue.isEmpty()) {
+      let size = queue.size();
+
+      while (size > 0) {
+        const node = queue.dequeue();
+
+        if (!node) {
+          size -= 1;
+          continue;
+        }
+
+        visit(node);
+
+        queue.enqueue(node.left);
+        queue.enqueue(node.right);
+
+        size -= 1;
+      }
+    }
+  }
+
+  dfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {
+    if (!node) return;
+
+    visit(node);
+    this.dfs(node.left, visit);
+    this.dfs(node.right, visit);
+  }
 }
