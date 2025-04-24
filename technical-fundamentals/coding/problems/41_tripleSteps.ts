8992d3a0701acd986ffe165137bdbd13d3bfedf3
Fhoerth <federicohoerth@gmail.com>
2025-04-24 13:04:36 -0300

    Add thriple steps impl

--- technical-fundamentals/coding/problems/41_tripleSteps.ts
+++ technical-fundamentals/coding/problems/41_tripleSteps.ts
@@ -5,4 +5,20 @@
 // how many possible ways the child can run up the stairs.
 //
 
-export default function tripleStep(n: number): number {}
+export default function tripleStep(n: number): number {
+  if (n <= 0) return 0;
+
+  let ppp: number = 0;
+  let pp: number = 0;
+  let curr: number = 1;
+
+  for (let j = 1; j <= n; j += 1) {
+    const next = ppp + pp + curr;
+
+    ppp = pp;
+    pp = curr;
+    curr = next;
+  }
+
+  return curr;
+}
