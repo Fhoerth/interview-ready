05ac8dbf7ef34033af94c34cac30911159b84ff2
Fhoerth <federicohoerth@gmail.com>
2025-04-24 10:56:14 -0300

    Add successor impl

--- technical-fundamentals/coding/problems/36_successor.ts
+++ technical-fundamentals/coding/problems/36_successor.ts
@@ -11,4 +11,20 @@ export type TreeNode<T> = {
   parent?: TreeNode<T>; // Link to parent node
 };
 
-export default function successor<T>(node: TreeNode<T>): TreeNode<T> | undefined {}
+function leftMost<T>(node: TreeNode<T> | undefined): TreeNode<T> | undefined {
+  if (!node) return undefined;
+  return leftMost(node?.left) || node;
+}
+
+export default function successor<T extends number>(node: TreeNode<T>): TreeNode<T> | undefined {
+  if (!node) return undefined;
+
+  if (node.right) return leftMost(node.right);
+  let currentNode: TreeNode<T> | undefined = node;
+
+  while (currentNode && currentNode.value <= node.value) {
+    currentNode = currentNode.parent;
+  }
+
+  return currentNode;
+}
--- technical-fundamentals/coding/problems/__tests__/trees/36_successor.test.ts
+++ technical-fundamentals/coding/problems/__tests__/trees/36_successor.test.ts
@@ -1,5 +1,12 @@
 import successor, { TreeNode } from '../../36_successor';
 
+function link<T>(parent: TreeNode<T>, child: TreeNode<T>, isLeft: boolean) {
+  child.parent = parent;
+
+  if (isLeft) parent.left = child;
+  else parent.right = child;
+}
+
 describe('successor', () => {
   test('returns correct in-order successor', () => {
     /*
@@ -39,5 +46,93 @@ describe('successor', () => {
   test('returns undefined for node without successor', () => {
     const node1: TreeNode<number> = { value: 1 };
     expect(successor(node1)).toBeUndefined();
+  });
+
+  test('handles deep and asymmetric structures', () => {
+    const node3: TreeNode<number> = { value: 3 };
+    const node5: TreeNode<number> = { value: 5, left: node3 };
+
+    const node13: TreeNode<number> = { value: 13 };
+    const node15: TreeNode<number> = { value: 15, left: node13 };
+
+    const node24: TreeNode<number> = { value: 24 };
+    const node26: TreeNode<number> = { value: 26 };
+    const node25: TreeNode<number> = { value: 25, left: node24, right: node26 };
+
+    const node30: TreeNode<number> = { value: 30, left: node25 };
+    const node20: TreeNode<number> = { value: 20, left: node15, right: node30 };
+
+    const node10: TreeNode<number> = { value: 10, left: node5, right: node20 };
+
+    link(node5, node3, true);
+    link(node10, node5, true);
+    link(node10, node20, false);
+    link(node20, node15, true);
+    link(node15, node13, true);
+    link(node20, node30, false);
+    link(node30, node25, true);
+    link(node25, node24, true);
+    link(node25, node26, false);
+
+    expect(successor(node3)?.value).toBe(5);
+    expect(successor(node5)?.value).toBe(10);
+    expect(successor(node10)?.value).toBe(13);
+    expect(successor(node13)?.value).toBe(15);
+    expect(successor(node15)?.value).toBe(20);
+    expect(successor(node20)?.value).toBe(24);
+    expect(successor(node24)?.value).toBe(25);
+    expect(successor(node25)?.value).toBe(26);
+    expect(successor(node26)?.value).toBe(30);
+    expect(successor(node30)).toBeUndefined();
+  });
+
+  test('chaotic successor test', () => {
+    const node5: TreeNode<number> = { value: 5 };
+    const node3: TreeNode<number> = { value: 3 };
+
+    const node13: TreeNode<number> = { value: 13 };
+    const node15: TreeNode<number> = { value: 15 };
+
+    const node24: TreeNode<number> = { value: 24 };
+    const node35: TreeNode<number> = { value: 35 };
+    const node40: TreeNode<number> = { value: 40 };
+
+    const node20: TreeNode<number> = { value: 20 };
+    const node10: TreeNode<number> = { value: 10 };
+
+    link(node10, node5, true);
+    link(node10, node20, false);
+    link(node5, node3, true);
+    link(node20, node15, true);
+    link(node15, node13, true);
+    link(node20, node40, false);
+    link(node40, node35, true);
+    link(node35, node24, true);
+
+    expect(successor(node3)?.value).toBe(5);
+    expect(successor(node5)?.value).toBe(10);
+    expect(successor(node10)?.value).toBe(13);
+    expect(successor(node13)?.value).toBe(15);
+    expect(successor(node15)?.value).toBe(20);
+    expect(successor(node20)?.value).toBe(24);
+    expect(successor(node24)?.value).toBe(35);
+    expect(successor(node35)?.value).toBe(40);
+    expect(successor(node40)).toBeUndefined();
+  });
+
+  test('increasing BST', () => {
+    const node1: TreeNode<number> = { value: 1 };
+    const node2: TreeNode<number> = { value: 2 };
+    const node3: TreeNode<number> = { value: 3 };
+    const node4: TreeNode<number> = { value: 4 };
+
+    link(node1, node2, false);
+    link(node2, node3, false);
+    link(node3, node4, false);
+
+    expect(successor(node1)?.value).toBe(2);
+    expect(successor(node2)?.value).toBe(3);
+    expect(successor(node3)?.value).toBe(4);
+    expect(successor(node4)?.value).toBeUndefined();
   });
 });
