454a9c57d339f1e0edb65abb8e5227f10e704e67
Fhoerth <federicohoerth@gmail.com>
2025-04-24 12:30:37 -0300

    Add power set impl

--- technical-fundamentals/coding/problems/44_powerSet.ts
+++ technical-fundamentals/coding/problems/44_powerSet.ts
@@ -6,4 +6,23 @@
 // Input: [1, 2, 3]
 // Output: [ [], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3] ];
 
-export function powerSet(set: number[]): number[][] {}
+export function powerSet(set: number[]): number[][] {
+  const solution: number[][] = [];
+
+  function backtrack(currentSolution: number[], currentIdx: number): void {
+    if (currentIdx === set.length) {
+      solution.push([...currentSolution]);
+      return;
+    }
+
+    currentSolution.push(set[currentIdx]);
+    backtrack(currentSolution, currentIdx + 1);
+    currentSolution.pop();
+
+    backtrack(currentSolution, currentIdx + 1);
+  }
+
+  backtrack([], 0);
+
+  return solution;
+}
