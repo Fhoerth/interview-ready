3e05bc8cb8f2e2acc11bec54b54437fe83f86068
Fhoerth <federicohoerth@gmail.com>
2025-04-24 06:33:57 -0300

    Remove route between nodes impl

--- technical-fundamentals/coding/problems/31_hasRouteBetweenNodes.ts
+++ technical-fundamentals/coding/problems/31_hasRouteBetweenNodes.ts
@@ -2,10 +2,35 @@
 
 // Given a directed graph, design an algorithm to find out whether there is a route
 // between two nodes.
+import { Queue } from '../ds/Queue';
 
 export type GraphNode = {
   value: number;
   neighbors: GraphNode[];
 };
 
-export default function hasRouteBetweenNodes(start: GraphNode, end: GraphNode): boolean {}
+/**
+ * Going to implement this by using bfs, since it's more efficient
+ *  and has less memory consumption.
+ * Plus: I love bfs :).
+ */
+export default function hasRouteBetweenNodes(start: GraphNode, end: GraphNode): boolean {
+  const queue = new Queue<GraphNode>();
+  const seen = new Set<GraphNode>();
+
+  queue.enqueue(start);
+  seen.add(start);
+
+  while (!queue.isEmpty()) {
+    const currentvertex = queue.dequeue()!;
+
+    if (currentvertex === end) return true;
+
+    for (const neighbor of currentvertex.neighbors) {
+      if (!seen.has(neighbor)) queue.enqueue(neighbor);
+      seen.add(neighbor);
+    }
+  }
+
+  return false;
+}
