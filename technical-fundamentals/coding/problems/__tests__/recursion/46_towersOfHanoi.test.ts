6c137a0e9545ba5d2ce814ddd060bb862997b0aa
Fhoerth <federicohoerth@gmail.com>
2025-04-30 16:29:18 -0300

    Skip towersOfHanoi

--- technical-fundamentals/coding/problems/__tests__/recursion/46_towersOfHanoi.test.ts
+++ technical-fundamentals/coding/problems/__tests__/recursion/46_towersOfHanoi.test.ts
@@ -1,6 +1,6 @@
 import { towersOfHanoi } from '../../46_towersOfHanoi';
 
-describe('towersOfHanoi', () => {
+describe.skip('towersOfHanoi', () => {
   test('returns correct tower configuration after moving disks', () => {
     const result1 = towersOfHanoi(3);
     expect(result1).toEqual([[], [], [3, 2, 1]]);
