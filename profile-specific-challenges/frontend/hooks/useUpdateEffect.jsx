4839781ceba555d47db5510fba546351950f8743
Fhoerth <federicohoerth@gmail.com>
2025-05-03 16:13:37 -0300

    Add useUpdateEffect hook

--- profile-specific-challenges/frontend/hooks/useUpdateEffect.jsx
+++ profile-specific-challenges/frontend/hooks/useUpdateEffect.jsx
@@ -1,1 +1,11 @@
-export function useUpdateEffect() {}
+import { useEffect } from 'react';
+import { useRendersCount } from './useRendersCount';
+
+export function useUpdateEffect(hook, deps) {
+  const count = useRendersCount();
+
+  useEffect(() => {
+    if (count <= 1) return;
+    hook();
+  }, [count, hook, deps]);
+}
