06f6a060c974dd3f53f487e50bb8e5ab1d480771
Fhoerth <federicohoerth@gmail.com>
2025-05-07 02:55:33 -0300

    Add Watch component

--- profile-specific-challenges/frontend/components/StopWatch/Watch.jsx
+++ profile-specific-challenges/frontend/components/StopWatch/Watch.jsx
@@ -1,53 +1,82 @@
-import { forwardRef, useImperativeHandle, useState, useCallback, useMemo } from "react";
-import { useInterval } from "../../hooks/useInterval";
+import { forwardRef, useImperativeHandle, useCallback, useState, useRef, useEffect } from "react";
+import { useAnimation } from "./useAnimation";
+import { useInterval } from '../../hooks/useInterval';
 
-const INTERVAL = 20;
+const DISPLAY_VALUE_INTERVAL = 100;
 const WATCH_SIZE = 200;
 const TICK_SIZE = 12;
 const BORDER_SIZE = 4;
-const SPEED = 1/5;
+const SPEED = 1 / 5;
+const WATCH_INITIAL_VALUE = 0;
 
 const Watch = forwardRef((_, ref) => {
   const [active, setActive] = useState(false);
-  const [displayValue, setDisplayValue] = useState(0);
+
+  const [displayValue, setDisplayValue] = useState(WATCH_INITIAL_VALUE);
+
+  const lastFrameTime = useRef(0);
+  const value = useRef(WATCH_INITIAL_VALUE);
+  const tickRef = useRef();
 
   const handleInterval = useCallback(() => {
     setDisplayValue((prevValue) => {
-      return prevValue + INTERVAL;
+      return prevValue + DISPLAY_VALUE_INTERVAL;
     });
   }, [setDisplayValue]);
 
-  const [start, stop] = useInterval(handleInterval, INTERVAL, false);
+  const [startInterval, stopInterval] = useInterval(handleInterval, DISPLAY_VALUE_INTERVAL, false);
+  const [startAnimation, stopAnimation] = useAnimation((now) => {
+    if (lastFrameTime.current === 0) {
+      lastFrameTime.current = now;
+    }
+
+    const { current } = value;
+
+    const angleInDegrees = (360 / 1000) * (current * SPEED);
+    const angleInRadians = (angleInDegrees * Math.PI) / 180;
+
+    const center = WATCH_SIZE / 2 - TICK_SIZE / 2;
+    const radius = center + BORDER_SIZE / 2 + TICK_SIZE / 2;
+
+    const x = center + Math.sin(angleInRadians) * radius;
+    const y = center - Math.cos(angleInRadians) * radius;
+
+    value.current += (now - lastFrameTime.current);
+
+    if (tickRef.current) {
+      tickRef.current.style.transform = `translate(${x}px, ${y}px)`;
+    }
+
+    lastFrameTime.current = now;
+  });
 
   useImperativeHandle(
     ref,
     () => ({
       start: () => {
         setActive(true);
-        start();
+        startInterval();
+        startAnimation();
       },
       pause: () => {
-        stop();
+        stopAnimation();
+        stopInterval();
+
+        value.current = displayValue;
+        lastFrameTime.current = 0;
       },
       reset: () => {
         setActive(false);
-        setDisplayValue(0);
-        stop();
+        stopInterval();
+        stopAnimation();
+
+        setDisplayValue(WATCH_INITIAL_VALUE);
+        value.current = WATCH_INITIAL_VALUE;
+        lastFrameTime.current = 0;
       },
     }),
-    [setActive, setDisplayValue, start, stop]
+    [setActive, setDisplayValue, startAnimation, stopAnimation]
   );
-
-  const fixedValue = (displayValue / 1000).toFixed(2);
-
-  const angleInDegrees = (360 / 1000) * (displayValue * SPEED);
-  const angleInRadians = (angleInDegrees * Math.PI) / 180;
-
-  const center = WATCH_SIZE / 2 - TICK_SIZE / 2;
-  const radius = center + BORDER_SIZE / 2 + TICK_SIZE / 2;
-
-  const x = center + Math.sin(angleInRadians) * radius;
-  const y = center - Math.cos(angleInRadians) * radius;
 
   const watchClassName = ["stop-watch__watch", active ? "stop-watch__watch-active" : null]
     .filter(Boolean)
@@ -59,8 +88,8 @@ const Watch = forwardRef((_, ref) => {
   return (
     <div className="stop-watch__watch-layout">
       <div className={watchClassName}>
-        {fixedValue}
-        <div className={tickClassName} style={{ left: x, top: y }} />
+        {(displayValue / 1000).toFixed(2)}
+        <div ref={tickRef} className={tickClassName} />
       </div>
     </div>
   );
